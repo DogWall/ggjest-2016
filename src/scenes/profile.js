3c5644072a96d613988040861c779d52d004ce85
Vincent Lark <vincent.lark@gmail.com>
2016-01-30 10:21:53 +0100

    better form

--- src/scenes/profile.js
+++ src/scenes/profile.js
@@ -31,16 +31,22 @@ define([
         },
 
         showForm: function () {
+
+            var self = this;
+
             this.panel = document.createElement('div');
+
+            var totalHeight = this.game.canvas.height;
+            var totalWidth  = this.game.canvas.width;
 
             var text = this.game.add.text(5, 50, 'Your name ?');
 
             var input = document.createElement('input');
-            input.type = 'text';
+            input.type           = 'text';
             input.style.position = 'absolute';
-            input.style.top      = '20%';
+            input.style.top      = totalHeight * .30;
             input.style.left     = 5;
-            input.style.width    = this.game.canvas.width - 10;
+            input.style.width    = totalWidth - 10;
             input.style.fontSize = '2em';
             input.addEventListener('keypressed', function (e) {
                 if (e.keyCode === 13) {
@@ -51,9 +57,9 @@ define([
             var button = document.createElement('button');
             button.innerHTML      = 'Enter';
             button.style.position = 'absolute';
-            button.style.top      = '60%';
+            button.style.top      = totalHeight * .50;
             button.style.left     = 5;
-            button.style.width    = this.game.canvas.width - 10;
+            button.style.width    = totalWidth - 10;
             button.style.fontSize = '2em';
             button.addEventListener('click', function (e) {
                 self.onConfirm(input.value);
